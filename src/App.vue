2608f2af727d90e0bec46cd1c8796303f8e35d3b
franciscoandraca <francisco.andraca14@gmail.com>
2023-10-18 19:20:11 -0600

    Creamos la barra de navegación

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,21 @@
 <template>
-      <h1 class="text-3xl font-bold underline">
-        Hola mundo!
-      </h1>
+  <div class="bg-blue-950 px-5 text-gray-400 py-2 text-sm">
+        <div class="container mx-auto flex justify-between">
+            <div class="flex items-center">
+                <div>
+                    Join our virtual event for Agencies & Ecommerce: Learn expert strategies to skyrocket your holiday sales
+                </div>              
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                  </svg>    
+            </div>
+            <div class="flex gap-x-4 items-center">
+                <a href="" class="bg-blue-800 px-3 py-1 rounded text-sky-400 font-semibold">We're Hiring</a>
+                <a href="" class="hover:text-white">Blog</a>
+                <a href="" class="hover:text-white">Docs</a>
+                <a href="" class="hover:text-white">Get Support</a>
+                <a href="" class="hover:text-white">Contact Sales</a>
+            </div>
+        </div>
+    </div>
 </template>
